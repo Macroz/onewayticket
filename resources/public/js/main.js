9ce27adb95c16f3189639372b14749dd38d5c59e
Markku Rontu <markku.rontu@iki.fi>
2012-04-21 10:50:36 +0300

    Cleaned up intro and outro and sorted schedule tasks

--- resources/public/js/main.js
+++ resources/public/js/main.js
@@ -5,21 +5,41 @@ function schedule(item) {
     todo.push(item);
 }
 
+function scheduleAt(time, fun) {
+    schedule({"time": time, "fun": fun});
+}
+
+function scheduleNow(fun) {
+    scheduleAt(currentTime, fun);
+}
+
+function scheduleAfter(time, fun) {
+    scheduleAt(currentTime + time, fun);
+}
+
 function scheduleSequenceOfText(seq) {
-    var t = currentTime;
     for (var i = 0; i < seq.length; i += 2) {
-        t += seq[i]
-        schedule({"time": t, "fun": displayLine(seq[i+1])});
+        scheduleAfter(seq[i], displayLine(seq[i+1]));
+    }
+}
+
+function log(text) {
+    if (console && console.log) {
+        console.log(text);
     }
 }
 
 function heartbeat(fast) {
     var speed = fast ? 10 : 1;
     return function() {
-        todo.sort();
+        todo.sort(function (o1, o2) { return o1.time - o2.time; });
         while (todo && todo.length > 0 && todo[0].time < currentTime) {
             var next = todo.shift();
-            next.fun();
+            if (next.fun) {
+                next.fun();
+            } else {
+                log("Problem executing " + next);
+            }
         }
         currentTime += speed;
     }
@@ -31,16 +51,26 @@ function displayLine(line) {
     };
 }
 
+function playSound(sound) {
+    var loop = new Audio("sounds/"+sound);
+    loop.preload = true;
+    loop.play();
+}
+
+function intro() {
+    scheduleNow(playSound("string-1-loop.wav"));
+    scheduleAfter(5, displayLine("But what about the Tiny World theme?"));
+    scheduleAfter(10, displayLine("You shall see!"));
+}
+
 function outro() {
+    scheduleNow(playSound("funeral.wav"));
     scheduleSequenceOfText([0, "Congratulations!", 5, "You have won the game!", 5, "Send feedback to markku.rontu@iki.fi or tweet!"]);
 }
 
+
 function init(fast) {
-    var loop = new Audio('sounds/string-1-loop.wav');
-    loop.preload = true;
-    loop.play();
     window.setInterval(heartbeat(fast), 1000);
-    schedule({"time": 5, "fun": displayLine("But what about the Tiny World theme?")});
-    schedule({"time": 10, "fun": displayLine("You shall see!")});
-    schedule({"time": 60, "fun": outro});
+    scheduleNow(intro);
+    scheduleAt(60, outro);
 }
